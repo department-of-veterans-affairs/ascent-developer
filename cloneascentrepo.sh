87cdc1fcb3178571b9851f51aae40c051daa764a
prashanthmds <prashanthmds@gmail.com>
2017-09-28 17:18:03 +0200

    Added the code to build the ascent-platform

--- cloneascentrepo.sh
+++ cloneascentrepo.sh
@@ -4,3 +4,8 @@ mkdir -p projects/workspace
 cd projects/workspace
 git clone https://github.com/department-of-veterans-affairs/ascent-platform.git
 echo '######## Ascent-Platform project repository cloned under /home/vagrant/projects/workspace ########' 
+cd ascent-platform
+git checkout development
+echo "\nBuilding the project ascent-platform"
+mvn clean install -DskipTests=true
+
